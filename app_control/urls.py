350f0da7a6498d4e35ca5dd2af7927b21b7b0795
EstefaniaPerezY <a01639270@itesm.mx>
2023-05-19 00:35:18 -0600

    try #1 import of the excel file

--- app_control/urls.py
+++ app_control/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path, include
 from .views import certificacionView
+from .views import excelImportView
 from rest_framework.routers import DefaultRouter
 
 router = DefaultRouter(trailing_slash=False)
@@ -7,4 +8,5 @@ router = DefaultRouter(trailing_slash=False)
 router.register('certificaciones', certificacionView, 'certificaciones')
 urlpatterns = [
     path('', include(router.urls)),
+    path('import/', excelImportView.as_view(), name='excel-import'),
 ]
--- app_control/views.py
+++ app_control/views.py
@@ -1,3 +1,7 @@
+from rest_framework.views import APIView
+from rest_framework.parsers import MultiPartParser
+import pandas as pd
+from .models import Certificaciones
 from rest_framework.viewsets import ModelViewSet
 from .serializers import (
     CertificacionesSerializer, Certificaciones
@@ -42,3 +46,30 @@ class certificacionView(ModelViewSet):
     def create(self, request, *args, **kwargs):
         
         return super().create(request, *args, **kwargs)
+    
+class excelImportView(APIView):
+    parser_classes = [MultiPartParser]
+
+    def post(self, request, *args, **kwargs):
+        file = request.FILES['file']  # Nombre del campo en el formulario de la solicitud
+
+        # Leer el archivo de Excel utilizando pandas
+        df = pd.read_excel(file)
+
+        # Realiza las operaciones necesarias con los datos del archivo
+        for _, row in df.iterrows():
+        # Create an instance of Certificaciones and populate it with data from the Excel row
+            instance = Certificaciones(
+                uid=row['uid'],
+                org=row['org'],
+                work_location=row['work_location'],
+                certifications=row['certification'],
+                issue_date=row['issue_date'],
+                type=row['type'],
+            )
+
+        # Save the instance to the database
+        instance.save()
+
+        # Devuelve una respuesta apropiada
+        return Response("Archivo de Excel importado correctamente.")
