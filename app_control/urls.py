70e80cee55a7aa0bd03d5f4595c37a7943860dec
EstefaniaPerezY <a01639270@itesm.mx>
2023-05-24 10:01:54 -0600

    i did something

--- app_control/urls.py
+++ app_control/urls.py
@@ -5,6 +5,10 @@ from rest_framework.routers import DefaultRouter
 
 router = DefaultRouter(trailing_slash=False)
 
-router.register('certificaciones', certificacionView, 'certificaciones')
+router.register('certificaciones', certificacionView, 'certificaciones'),
 router.register('upload', CertificationsCSVLoaderView, 'upload')
 
+urlpatterns = [
+    # Other URL patterns in your urls.py file
+    path('', include(router.urls)),
+]
